aac900c8307a46b4a76a3899fdd33dc754332bd7
nut <suppanat.nut@gmail.com>
2024-04-27 10:42:09 +0700

    fix js-oop-ex-1,2

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,31 @@
-class Notification { }
+class Notification { 
+    constructor(notificationId,createdTime,content,receiver) {
+        this.notificationId = notificationId;
+        this.createdTime = createdTime;
+        this.content = content;
+        this.receiver= receiver;
+    }
+    send() {
+        console.log(`Notification has been sent to ${this.receiver}`);
+    }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification{ 
+    constructor(notificationId,createdTime,content,receiver) {
+        super(notificationId,createdTime,content,receiver)
+    }
+  
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification { 
+    constructor(notificationId,createdTime,content,receiver) {
+        super(notificationId,createdTime,content,receiver)
+    }
+  
+}
+
+const myEmail = new EmailNotification("12345","12:00:00","Hello","Nttozang.mail.com");
+myEmail.send();
+
+const myphoneNumber = new EmailNotification("12345","12:00:00","Hello","0987321456");
+myphoneNumber .send();
